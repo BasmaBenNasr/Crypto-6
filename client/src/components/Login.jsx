8dc7385ff85cba22923a60f72c57ab6054af181c
BasmaBenNasr <bennasrbasma97@gmail.com>
2020-10-18 06:35:37 +0200

    starting to refactor code

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,78 +1,57 @@
 import React, { Component } from "react";
 import axios from "axios";
 
-export default class Login extends Component {
-  constructor(props) {
+export default class Login extends Component{
+  constructor(props){
     super(props);
-
-    this.state = {
-      email: "",
-      password: "",
-      loginErrors: ""
+    this.state={
+      email:"",
+      password:"",
+      loginErrors:""
+ 
     };
+this.handleSubmit = this.handleChange.bind(this);
+this.handleChange = this.handleChange.bind(this);
+}
+handleChange(event){
+  this.setState({
+    [event.target.name] : event.target.value
+  });
+}
+handleSubmit(event){
+  const {email,password}=this.state;
+  axios
+  .post(
+    "http://localhost:3001.sessions",
+    {
+      user:{
+        email:email,
+        password:password,
+       
+      }
+    },
+   { withCredentials:true}
+  )
+  .then(response=>{
+    if(response.data.logged_in){
+      this.props.handleSuccessfulAuth(response.data);
+    }
+  })
+  .catch(error=>{
+    console.log("login error", error)
+  });
+  event.preventDefault();
+}
 
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-  }
-
-  handleSubmit(event) {
-    const { email, password } = this.state;
-
-    axios
-      .post(
-        "http://localhost:3001/sessions",
-        {
-          user: {
-            email: email,
-            password: password
-          }
-        },
-        { withCredentials: true }
-      )
-      .then(response => {
-        if (response.data.logged_in) {
-          this.props.handleSuccessfulAuth(response.data);
-        }
-      })
-      .catch(error => {
-        console.log("login error", error);
-      });
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <input 
-            className = "input1"
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            required
-          />
-
-          <input
-              className = "input2"
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            required
-          />
-
-          <button className="login"type="submit">Login</button>
+render(){
+  return(
+    <div>
+       <form onSubmit={this.handleSubmit}>
+       <input className="input1" type="email" name="email" placeholder="Email" value = {this.state.email} onChange={this.handleChange}required/>
+       <input className="input2" type="password" name="password" placeholder="Password" value = {this.state.password} onChange={this.handleChange}required/>
+        <button className="login" type="submit">Login</button>
         </form>
-      </div>
-    );
-  }
+    </div>
+  );
+}
 }
--- client/src/components/Registration.jsx
+++ client/src/components/Registration.jsx
@@ -1,90 +1,63 @@
 import React, { Component } from "react";
 import axios from "axios";
-
-export default class Registration extends Component {
-  constructor(props) {
+import { extend } from "jquery";
+import { response } from "express";
+export default class Registration extends Component{
+  constructor(props){
     super(props);
-
-    this.state = {
-      email: "",
-      password: "",
-      password_confirmation: "",
-      registrationErrors: ""
+    this.state={
+      email:"",
+      password:"",
+      password_confirmation:"",
+      registrationErrors:""
     };
-
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
-
-  handleChange(event) {
+  handleChange(event){
     this.setState({
-      [event.target.name]: event.target.value
+      [event.target.name] : event.target.value
     });
   }
-
-  handleSubmit(event) {
-    const { email, password, password_confirmation } = this.state;
-
+  handleSubmit(event){
+    const {email,password,password_confirmation}=this.state;
     axios
-      .post(
-        "http://localhost:3001/registrations",
-        {
-          user: {
-            email: email,
-            password: password,
-            password_confirmation: password_confirmation
-          }
-        },
-        { withCredentials: true }
-      )
-      .then(response => {
-        if (response.data.status === "created") {
-          this.props.handleSuccessfulAuth(response.data);
+    .post(
+      "http://localhost:3000.registration",
+      {
+        user:{
+          email:email,
+          password:password,
+          password_confirmation: password_confirmation
         }
-      })
-      .catch(error => {
-        console.log("registration error", error);
-      });
+      },
+     { withCredentials:true}
+    )
+    .then(response=>{
+      if(response.data.status==="created"){
+        this.props.handleSuccessfulAuth(response.data);
+      }
+    })
+    .catch(error=>{
+      console.log("registration error", error)
+    });
     event.preventDefault();
   }
-
-  render() {
-    return (
+  render(){
+    return(
       <div>
         <form onSubmit={this.handleSubmit}>
-          <input
-          className="reg1"
-            type="email"
-            name="email"
-            placeholder="Email"
-            value={this.state.email}
-            onChange={this.handleChange}
-            required
-          />
-
-          <input
-          className="reg2"
-            type="password"
-            name="password"
-            placeholder="Password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            required
-          />
-
-          <input
-          className="reg3"
-            type="password"
-            name="password_confirmation"
-            placeholder="Password confirmation"
-            value={this.state.password_confirmation}
-            onChange={this.handleChange}
-            required
-          />
-
-          <button className="bt"type="submit">Register</button>
+          <input className="reg1" type="email" placeholder="Email" value = {this.state.email} onChange={this.handleChange}required/>
+          <input className="reg2" type="password" placeholder="Passowrd" value = {this.state.password} onChange={this.handleChange}required/>
+          <input className="reg3" type="password" placeholder="Password confirmation" value = {this.state.password_confirmation} onChange={this.handleChange}required/>
+          <button className="bt" type="submit">Register</button>
         </form>
       </div>
     );
   }
 }
+
+
+    
+
+   
